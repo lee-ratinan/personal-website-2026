6623c600f6d732d929f45de533afca0d5be5b8d3
Ratinan Lee <lee.ratinan@gmail.com>
2025-11-28 17:28:24 +0800

    fix shavian fonts

--- app/Views/home.php
+++ app/Views/home.php
@@ -13,14 +13,16 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <?php if ('th' == $locale) : ?>
+    <?php if ('en' == $locale) : ?>
+        <link href="https://fonts.googleapis.com/css2?family=Rampart+One&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <?php elseif ('th' == $locale) : ?>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&family=Sriracha&display=swap" rel="stylesheet">
     <?php elseif ('zh-TW' == $locale) : ?>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Stick&display=swap" rel="stylesheet">
     <?php elseif ('ja' == $locale) : ?>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200..900&family=Rampart+One&display=swap" rel="stylesheet">
     <?php else : ?>
-        <link href="https://fonts.googleapis.com/css2?family=Rampart+One&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Shavian&display=swap" rel="stylesheet">
     <?php endif; ?>
     <!-- Vendor CSS Files -->
     <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
